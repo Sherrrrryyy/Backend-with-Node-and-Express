d0f04c010b3d5fece3dd682975c238805b030e90
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-01-09 04:06:56 +0500

    Add CRUD endpoints for root route in index.js and test functions with postman

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ app.get('/', (req, res) => {
     res.send("Hello world of the backend")
 })
 
+app.post('/', (req, res) => {
+    // console.log(req);
+    res.send("Post request has been called")
+})
+
+app.put('/', (req, res) => {
+    // console.log(req);
+    res.send("Put request has been called")
+})
+
+app.delete('/', (req, res) => {
+    // console.log(req);
+    res.send("Delete request has been called")
+})
+
 app.listen(PORT, () => console.log("Server is running on PORT" + PORT));
