794e0b72f11674b5b9accb6e4d14abfea1919e41
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-01-09 03:37:45 +0500

    Set up Express server and update package.json with start scripts

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("welcome to the backend")
+
+
+import express from 'express';
+const app = express();
+const PORT = 4000;
+
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send("Hello world of the backend")
+})
+
+app.listen(PORT, () => console.log("Server is running on PORT" + PORT));
